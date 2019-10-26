d4fa7a56cc9a2de15983543581c25003f1e689a0
Deya Genao <deyagenao@gmail.com>
2019-10-25 20:31:43 -0400

    updated server file to include code for including express and handlebars and starting server

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,14 +1,32 @@
 //===========================================================================================
 // DEPENDENCIES 
 //===========================================================================================
+
 var express = require("express");
+
+// Import the model (burger.js) to use its database functions 
 var burger = require("../models/burger");
+
 
 //===========================================================================================
 // ROUTER
 //===========================================================================================
-var router;
+
+// Using express to create the router
+var router = express.Router();
+
+
+// Create all routes
+// Home route: executes the selectAll function to receive and display all burger data from the database 
+router.get("/", function(req, res){
+    burger.selectAll(function(data){
+        var hbsObject = {
+            burgers: data
+        }
+    })
+    res.render("index", hbsObject)
+})
 
 
 // Export router 
-module.exports = router
+module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,35 @@
-//============================================================
-// DEPENDENCIES 
-//============================================================
 // require("dotenv").config();
+
+// Set up express 
 var express = require("express");
+
+// PORT variable assigned a value of process.env.PORT for deployment on Heroku, or 8080 to be viewed locally
+var PORT = process.env.PORT || 8080;
+
 var app = express();
+
+// Express middleware for serving static files from the public directory 
+app.use(express.static("public"));
+
+
+// Express middleware for parsing data, strings and JSON objects 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Set up handlebars 
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+
+// Import router and give server access to them 
+var routes = require("./controllers/burgers_controller.js");
+
+app.use(routes);
+
+// Start the server and listen to client requests 
+app.listen(PORT, function() {
+    console.log("Server listening on: http://localhost:", PORT);
+});
+
