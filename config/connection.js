e5776cead44897b5dd7fc0652ab2ce33f6b997ed
Deya Genao <deyagenao@gmail.com>
2019-10-28 19:35:47 -0400

    updated connection to allow for deployment using heroku and jawsdb

--- config/connection.js
+++ config/connection.js
@@ -2,17 +2,20 @@
 // DEPENDENCIES 
 //============================================================
 var mysql = require("mysql");
-
+var connection;
 
 // Set up mySQL connection 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user:"root",
-    password: "AnotherPassword",
-    database: "burgers_db"
-});
-
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else{
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user:"root",
+        password: "AnotherPassword",
+        database: "burgers_db"
+    });
+};
 
 // Start mySQL connection 
 connection.connect(function(err){
